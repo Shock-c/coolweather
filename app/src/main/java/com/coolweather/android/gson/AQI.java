78fd9ed7a364b67dbef7aba774bf6ad9747cc7dc
shock <shock@gmail.com>
2018-01-17 17:40:13 +0800

    完成版（存在刷新bug）

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by shock on 2018/1/17.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+
+}
